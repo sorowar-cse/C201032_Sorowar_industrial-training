c76f19adda64167b4b270b8359a492d060787862
Sorowar Mahabub <76944781+sorowar-cse@users.noreply.github.com>
2024-08-31 02:12:34 +0600

    Final Work doneeeeeeeeeeeeeeeeee

---
+++ Final_Work/streamlit-app/Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="News and Summary App (by Team 1)", layout="wide")
+
+st.header("News Summarization App (Re-edited by Sorowar, C201032, Team 1)")
+st.subheader("AI based Prothomalo news summarization")
+st.markdown("This app allows you to view news articles and generate summaries.")
